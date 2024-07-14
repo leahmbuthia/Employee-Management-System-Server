04fd87dc6ae2e3f1fb0ca4613350498ac3b75e32
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-14 23:20:44 +0300

    employee count route

--- Routes/AdminRoute.js
+++ Routes/AdminRoute.js
@@ -118,6 +118,13 @@ router.delete('/delete_employee/:id', (req, res) => {
         return res.json({Status: true, Result: result})
     })
 })
+router.get('/employee_count', (req, res) => {
+    const sql = "select count(id) as employee from employee";
+    con.query(sql, (err, result) => {
+        if(err) return res.json({Status: false, Error: "Query Error"+err})
+        return res.json({Status: true, Result: result})
+    })
+})
 router.get('/salary_count', (req, res) => {
     const sql = "select sum(salary) as salaryOFEmp from employee";
     con.query(sql, (err, result) => {
