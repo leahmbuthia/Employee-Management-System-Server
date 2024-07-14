0c33aa2c0133f20aed2693f17258b84ec40a673b
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-14 23:54:19 +0300

    Employee LogIn

--- Routes/EmployeeRoute.js
+++ Routes/EmployeeRoute.js
@@ -29,19 +29,3 @@ router.post("/employee_login", (req, res) => {
       }
     });
   });
-
-  router.get('/detail/:id', (req, res) => {
-    const id = req.params.id;
-    const sql = "SELECT * FROM employee where id = ?"
-    con.query(sql, [id], (err, result) => {
-        if(err) return res.json({Status: false});
-        return res.json(result)
-    })
-  })
-
-  router.get('/logout', (req, res) => {
-    res.clearCookie('token')
-    return res.json({Status: true})
-  })
-
-  export {router as EmployeeRouter}
