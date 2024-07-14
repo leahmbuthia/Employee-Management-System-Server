0bb5b685f4d05006c9e17d24541af6b970353357
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-15 01:24:22 +0300

    verify User

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { adminRouter } from "./Routes/AdminRoute.js";
 import { EmployeeRoute } from "./Routes/EmployeeRoute.js";
 import cors from 'cors'
+import  Jwt from 'jsonwebtoken'
+// import cookieParser from "cookie-parser";
 
 
 const app =express()
@@ -11,9 +13,29 @@ app.use(cors({
     credentials: true // Allow credentials to be sent
 }));
 app.use(express.json())
+// app.use(cookieParser)
 app.use('/auth',adminRouter);
 app.use('/employee',EmployeeRoute);
 app.use(express.static('Public'))
+
+const verifyUser =(req,res,next)=>{
+    const token =req.cookies.token;
+    if(token){
+      Jwt.verify(token,"jwt_secret_key",(err,decoded) =>{
+        if(err) return res.json({Status:false, Error:"Wrong Token"})
+            req.id=decoded.id;
+           req.role=decoded.role;
+           next();
+
+      })
+      
+    }else{
+        return res.json({Status:false, Error:"Not autheticated"})
+    }
+}
+app.get('verify',verifyUser,(req,res)=>{
+    return res.json({Status:true, role: req.role, id: req.id})
+})
 
 app.listen(3000, ()=>{
     console.log("server running");
